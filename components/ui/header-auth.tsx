1e828091a3e22bd55fd0ec82a6a01b80bf54e6af
Thomas Davis <thomasalwyndavis@gmail.com>
2025-04-15 23:05:15 +1000

    refactored header

--- components/ui/header-auth.tsx
+++ components/ui/header-auth.tsx
@@ -1,7 +1,5 @@
 import { signOutAction } from "@/app/actions";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import Link from "next/link";
-import { Badge } from "./badge";
 import { Button } from "./button";
 import { createClient } from "@/utils/supabase/server";
 import { UserCircle, LogOut } from "lucide-react";
@@ -14,42 +12,7 @@ export default async function AuthButton() {
     data: { user },
   } = await supabase.auth.getUser();
 
-  if (!hasEnvVars) {
-    return (
-      <>
-        <div className="flex gap-4 items-center">
-          <div>
-            <Badge
-              variant={"default"}
-              className="font-normal pointer-events-none"
-            >
-              Please update .env.local file with anon key and url
-            </Badge>
-          </div>
-          <div className="flex gap-2">
-            <Button
-              asChild
-              size="sm"
-              variant={"outline"}
-              disabled
-              className="opacity-75 cursor-none pointer-events-none"
-            >
-              <Link href="/sign-in">Sign in</Link>
-            </Button>
-            <Button
-              asChild
-              size="sm"
-              variant={"default"}
-              disabled
-              className="opacity-75 cursor-none pointer-events-none"
-            >
-              <Link href="/sign-up">Sign up</Link>
-            </Button>
-          </div>
-        </div>
-      </>
-    );
-  }
+
   return user ? (
     <div className="flex items-center gap-3">
       <span className="text-sm text-muted-foreground">
@@ -78,7 +41,7 @@ export default async function AuthButton() {
       <Button asChild size="sm" variant={"outline"}>
         <Link href="/sign-in">Sign in</Link>
       </Button>
-      <Button asChild size="sm" variant={"default"} className="text-white dark:text-primary-foreground">
+      <Button asChild size="sm" variant={"default"} className="text-black dark:text-primary-foreground">
         <Link href="/sign-up">Sign up</Link>
       </Button>
     </div>
