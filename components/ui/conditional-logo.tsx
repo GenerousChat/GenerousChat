6106d15cb173175705aa351ce622fc9374ce4040
davincidreams <lisamegawatts@gmail.com>
2025-04-16 22:05:08 -0400

    logo ug

--- components/ui/conditional-logo.tsx
+++ components/ui/conditional-logo.tsx
@@ -19,7 +19,7 @@ export default function ConditionalLogo() {
       href="/" 
       className="flex items-center gap-2 pt-3 pb-2 px-2 text-xl font-bold tracking-tight hover:text-primary transition-colors"
     >
-      <img src="/logo.svg" alt="Logo" className="h-12 dark:invert dark:brightness-200" />
+      <img src="/logo.svg" alt="Logo" className="h-12 viewbox-h-16 dark:invert dark:brightness-200" />
     </Link>
   );
 } 
